05327da2e7e69a10a44f9d270093bcc52f41cf2d
maxhoare61 <maxhoare61@gmail.com>
2025-01-02 01:29:13 +1100

    Updated design and scrolling

--- script.js
+++ script.js
@@ -1,90 +1,56 @@
-// using d3 for convenience
-var container = d3.select("#scroll");
-var graphic = container.select(".scroll__graphic");
-var chart = graphic.select(".chart");
-var text = container.select(".scroll__text");
-var step = text.selectAll(".step");
+// Select elements
+var scrolly = d3.select("#scrolly");
+var figure = scrolly.select("figure");
+var article = scrolly.select("article");
+var step = article.selectAll(".step");
 
-// initialize the scrollama
+// Initialize Scrollama
 var scroller = scrollama();
 
-// generic window resize listener event
+// Resize handler
 function handleResize() {
-  // 1. update height of step elements
   var stepHeight = Math.floor(window.innerHeight * 0.75);
   step.style("height", stepHeight + "px");
 
-  // 2. update width/height of graphic element
-  var bodyWidth = d3.select("body").node().offsetWidth;
+  var figureHeight = window.innerHeight / 2;
+  var figureMarginTop = (window.innerHeight - figureHeight) / 2;
 
-  graphic
-    .style("width", bodyWidth + "px")
-    .style("height", window.innerHeight + "px");
+  figure.style("height", figureHeight + "px").style("top", figureMarginTop + "px");
 
-  var chartMargin = 32;
-  var textWidth = text.node().offsetWidth;
-  var chartWidth = graphic.node().offsetWidth - textWidth - chartMargin;
-
-  chart
-    .style("width", chartWidth + "px")
-    .style("height", Math.floor(window.innerHeight / 2) + "px");
-
-  // 3. tell scrollama to update new element dimensions
   scroller.resize();
 }
 
-// scrollama event handlers
+// Step enter event handler
 function handleStepEnter(response) {
-  // response = { element, direction, index }
-
-  // add color to current step only
-  step.classed("is-active", function(d, i) {
+    
+  step.classed("is-active", function (d, i) {
     return i === response.index;
   });
 
-  // update graphic based on step
-  chart.select("p").text(response.index + 1);
+  const numberElement = figure.select("p");
+
+  if (parseInt(numberElement.text(), 10) === response.index + 1) {
+    return;
+  }
+
+  // Fade out
+  numberElement.text(response.index + 1);
 }
 
-function handleContainerEnter(response) {
-  // response = { direction }
-  console.log("Container entered");
-  // sticky the graphic (old school)
-  graphic.classed("is-fixed", true);
-  graphic.classed("is-bottom", false);
-}
-
-function handleContainerExit(response) {
-  // response = { direction }
-  console.log("Container exited", response.direction);
-  
-  // un-sticky the graphic, and pin to top/bottom of container
-  graphic.classed("is-fixed", false);
-  graphic.classed("is-bottom", response.direction === "down");
-}
-
+// Initialize everything
 function init() {
-  // 1. force a resize on load to ensure proper dimensions are sent to scrollama
   handleResize();
 
-  // 2. setup the scroller passing options
-  // this will also initialize trigger observations
-  // 3. bind scrollama event handlers (this can be chained like below)
   scroller
     .setup({
-      container: "#scroll",
-      graphic: ".scroll__graphic",
-      text: ".scroll__text",
-      step: ".scroll__text .step",
-      debug: true
+      step: "#scrolly article .step",
+      offset: 0.33,
+      debug: false,
     })
-    .onStepEnter(handleStepEnter)
-    .onContainerEnter(handleContainerEnter)
-    .onContainerExit(handleContainerExit);
+    .onStepEnter(handleStepEnter);
 
-  // setup resize event
   window.addEventListener("resize", handleResize);
 }
 
-// kick things off
+// Kick off the initialization
 init();
